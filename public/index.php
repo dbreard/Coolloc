905e1495b91a73a1fc92cb0fcaa3aba6f29b3d32
dbreard <davidbreard@gmail.com>
2018-01-12 13:29:01 +0100

    modif layout

--- public/index.php
+++ public/index.php
@@ -5,7 +5,22 @@
     require_once __DIR__.'/../vendor/autoload.php';
 
     use Silex\Application;
+
     $app = new Application();
+
+    //----------------------------------------------------------------------------------------------------------------------
+    $app['debug'] = true; // a supprimer en mode prod
+    //----------------------------------------------------------------------------------------------------------------------
+    
     require __DIR__.'/../src/register.php';
+    // require __DIR__.'/../src/function.php';
+    // require __DIR__.'/../src/middleware.php';
+
+    // require __DIR__.'/../src/model/TokensDAO.php';
+
+    // require __DIR__.'/../src/controller/Controller.php';
+    // require __DIR__.'/../src/controller/IndexController.php';
+
     require __DIR__.'/../src/route.php';
+
     $app->run();
