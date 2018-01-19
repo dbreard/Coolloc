5125e35938ef8c494d867a8f80e54cfa7e889a9f
Michou <regent.mickael.mai@gmail.com>
2018-01-19 15:36:37 +0100

    Page profil
    changement de la requete de selection des annonce pour l'affichage dans le profil

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -36,7 +36,7 @@ class Model {
 
     //SELECTION DES ANNONCES CORRESPONDANT A SON PROPRIETAIRE (INFOS DE BASE)
     public static function annonceByUser(string $idUser, Application $app){
-      $sql = "SELECT a.name_coloc, a.description, m.url_media FROM annonce_options_city a, media m WHERE a.id_user_post_annonce = m.user_post_annonce_id AND user_id = ?";
+      $sql = "SELECT a.name_coloc, a.description, m.url_media FROM annonce_options_city a, media m WHERE a.id_user_post_annonce = m.user_post_annonce_id AND user_id = ? GROUP BY a.id_user_post_annonce";
       $userAnnonce = $app['db']->fetchAll($sql, array((string) $idUser));
 
       return $userAnnonce;
