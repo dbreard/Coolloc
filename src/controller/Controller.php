24d289441c415bc1f955fd7066947d018e2fc24e
ThibaultGuinet45 <zaraki41@gmail.com>
2018-01-22 18:11:33 +0100

    debug page contact

--- src/controller/Controller.php
+++ src/controller/Controller.php
@@ -337,7 +337,7 @@ class Controller {
         global $app;
         $mail = $app['mail'];
         //Server settings
-        $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+        $mail->SMTPDebug = 0;                                 // Enable verbose debug output
         $mail->isSMTP();                                      // Set mailer to use SMTP
         $mail->Host = 'smtp-mail.outlook.com';                       // Specify main and backup SMTP servers
         $mail->SMTPAuth = true;                               // Enable SMTP authentication
